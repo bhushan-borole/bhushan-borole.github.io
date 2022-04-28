5a6f9dc0e624759a1844a856e8f7682eea0f0136
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-28 16:10:56 +0100

    Add latest project Nuharc

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,78 +4,68 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function About() {
-    return (
-        <section id="about" class="about background-alt">
-            <Container data-aos="fade-up">
-                <div class="section-title">
-                    <h2>About</h2>
-                </div>
+  return (
+    <section id="about" class="about background-alt">
+      <Container data-aos="fade-up">
+        <div class="section-title">
+          <h2>About</h2>
+        </div>
 
-                <Row>
-                    <Col lg={4} className="d-flex justify-content-center">
-                        <img
-                            src={img}
-                            height="100%"
-                            width="60%"
-                            style={{ borderRadius: "50%" }}
-                        />
-                    </Col>
-                    <Col lg={8} pt={4} className="content">
-                        <br></br>
-                        <h3>Software Developer.</h3>
-                        <br></br>
-                        <p style={{ color: "#728394" }}>
-                            A passionate Full Stack Developer, Competitive
-                            Programmer, ML enthusiast and Tabla player.
-                        </p>
-                        <br></br>
-                        <p style={{ color: "#728394" }}>
-                            Always seeking challenging position in and
-                            Organization where my skills find ample of
-                            oppurtunities for upgradation and growth of the
-                            company.
-                        </p>
-                        <br></br>
-                        <Row>
-                            <Col lg={4}>
-                                <ul>
-                                    <li>
-                                        <i class="bi bi-chevron-right"></i>{" "}
-                                        <strong>Phone:</strong>{" "}
-                                        <span>+353 0894010329</span>
-                                    </li>
-                                    <li>
-                                        <i class="bi bi-chevron-right"></i>{" "}
-                                        <strong>City:</strong>{" "}
-                                        <span>Dublin, Ireland</span>
-                                    </li>
-                                </ul>
-                            </Col>
-                            <Col lg={7}>
-                                <ul>
-                                    <li>
-                                        <i class="bi bi-chevron-right"></i>{" "}
-                                        <strong>Degree:</strong>{" "}
-                                        <span>
-                                            MSc - Intelligent Systems @ TCD
-                                            (2021-2022)
-                                        </span>
-                                    </li>
-                                    <li>
-                                        <i class="bi bi-chevron-right"></i>{" "}
-                                        <strong>Email:</strong>{" "}
-                                        <span>
-                                            <a href="mailto:boroleb@tcd.ie">
-                                                boroleb@tcd.ie
-                                            </a>
-                                        </span>
-                                    </li>
-                                </ul>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+        <Row>
+          <Col lg={4} className="d-flex justify-content-center">
+            <img
+              src={img}
+              height="100%"
+              width="50%"
+              style={{ borderRadius: "50%" }}
+            />
+          </Col>
+          <Col lg={8} pt={4} className="content">
+            <br></br>
+            <h3>Software Developer.</h3>
+            <br></br>
+            <p style={{ color: "#728394" }}>
+              A passionate Full Stack Developer, Competitive Programmer, ML
+              enthusiast and Tabla player.
+            </p>
+            <br></br>
+            <p style={{ color: "#728394" }}>
+              Always seeking challenging position in and Organization where my
+              skills find ample of oppurtunities for upgradation and growth of
+              the company.
+            </p>
+            <br></br>
+            <Row>
+              <Col lg={4}>
+                <ul>
+                  <li>
+                    <i class="bi bi-chevron-right"></i> <strong>Phone:</strong>{" "}
+                    <span>+353 0894010329</span>
+                  </li>
+                  <li>
+                    <i class="bi bi-chevron-right"></i> <strong>City:</strong>{" "}
+                    <span>Dublin, Ireland</span>
+                  </li>
+                </ul>
+              </Col>
+              <Col lg={7}>
+                <ul>
+                  <li>
+                    <i class="bi bi-chevron-right"></i> <strong>Degree:</strong>{" "}
+                    <span>MSc - Intelligent Systems @ TCD (2021-2022)</span>
+                  </li>
+                  <li>
+                    <i class="bi bi-chevron-right"></i> <strong>Email:</strong>{" "}
+                    <span>
+                      <a href="mailto:boroleb@tcd.ie">boroleb@tcd.ie</a>
+                    </span>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
